e188f701627bc570d8f5073fc25b274b5b37f253
Rajeev <rajeevkr8917@gmail.com>
2024-02-15 17:19:53 +0530

    protected route and message model/api

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,9 @@
 import express from "express";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.routes.js";
+import messageRoutes from "./routes/message.routes.js";
 import connectToMongoDB from "./db/connectToMongoDB.js";
+import cookieParser from "cookie-parser";
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -9,8 +11,10 @@ const PORT = process.env.PORT || 5000;
 dotenv.config();
 
 app.use(express.json());    //to parse the incoming request with JSON payload(from req.body)
+app.use(cookieParser());    //to parse the cookie
 
 app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoutes);
 
 // app.get("/", (req, res) => {
 //     res.send("Hello World!!");
