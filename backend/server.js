9c1fd3a07348e40f79b7482fe12684fafe8d916e
Rajeev <rajeevkr8917@gmail.com>
2024-02-15 12:51:17 +0530

    user model,routes and signup

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,20 @@
 import express from "express";
 import dotenv from "dotenv";
+import authRoutes from "./routes/auth.routes.js";
 import connectToMongoDB from "./db/connectToMongoDB.js";
 
 const app = express();
-
-dotenv.config();
 const PORT = process.env.PORT || 5000;
 
-app.get("/", (req, res) => {
-    res.send("Hello World!!");
-});
+dotenv.config();
 
+app.use(express.json());    //to parse the incoming request with JSON payload(from req.body)
+
+app.use("/api/auth", authRoutes);
+
+// app.get("/", (req, res) => {
+//     res.send("Hello World!!");
+// });
 
 app.listen(PORT, () => {
     connectToMongoDB();
