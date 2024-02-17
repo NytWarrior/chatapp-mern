b00795e6efd75735b9a7c3fe39e60196b7333574
Rajeev <rajeevkr8917@gmail.com>
2024-02-18 03:01:20 +0530

    socket.io with some css

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,8 @@ import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
 import usersRoutes from "./routes/user.routes.js";
 
-const app = express();
+import { app, server } from "./socket/socket.js";
+
 const PORT = process.env.PORT || 5000;
 
 dotenv.config();
@@ -23,7 +24,7 @@ app.use("/api/users", usersRoutes);
 //     res.send("Hello World!!");
 // });
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
 	connectToMongoDB();
 	console.log(`Server running on port ${PORT}`);
 })
